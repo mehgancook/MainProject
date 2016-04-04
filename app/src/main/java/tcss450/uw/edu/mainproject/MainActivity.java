a58e84b2cb11bf42395e0a68ec799213a6fb2c42
anromeo <anthonykzullo@gmail.com>
2016-04-04 15:17:51 -0700

    Added a button

--- app/src/main/java/tcss450/uw/edu/mainproject/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/mainproject/MainActivity.java
@@ -1,13 +1,14 @@
 package tcss450.uw.edu.mainproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void press(View view) {
+        Intent intent = new Intent(this, ButtonPress.class);
+        startActivity(intent);
     }
 }
