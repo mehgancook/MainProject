394d6d5027aa2d68fb2aafbfc1672bb3219dc6da
Mehgan Cook <mehgancook@yahoo.com>
2016-04-17 15:56:05 -0700

    added some facebook functionality

--- app/src/main/java/tcss450/uw/edu/mainproject/Splash.java
+++ app/src/main/java/tcss450/uw/edu/mainproject/Splash.java
@@ -1,7 +1,12 @@
 package tcss450.uw.edu.mainproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import android.view.View;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+
+    public void toLoginPage(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void toFacebook(View view) {
+
+       // Intent intentFacebook = new Intent(this, .class);
+       // startActivity(intentImage);
+    }
+
+    public void toJoin(View view) {
+        Intent intentJoin = new Intent(this, Join.class);
+        startActivity(intentJoin);
+
     }
 }
