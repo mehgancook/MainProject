1be7367323407b9e7114b54fc693393560f8e50f
Mehgan Cook <mehgancook@yahoo.com>
2016-04-17 16:14:46 -0700

    added some facebook functionality

---
+++ app/src/main/java/tcss450/uw/edu/mainproject/Join.java
@@ -0,0 +1,17 @@
+package tcss450.uw.edu.mainproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+
+
+
+
+}
